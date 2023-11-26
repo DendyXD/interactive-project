1154b32a4b74a97bb81daebec92118d1f989dcc0
Vanya <i.boychyshyn@gmail.com>
2023-11-26 22:28:17 +0200

    third commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './js/header'
+import './js/footer'
+
+import './js/game1'
+import './js/game2'
+import './js/game3'
+import './js/game4'
+import './js/game5'
+import './js/game6'
+import './js/game7'
+import './js/game8'
+import './js/game9'
+import './js/slider'
